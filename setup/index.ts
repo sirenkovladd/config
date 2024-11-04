f89f6352b73a7c8eb60166a2253051bf505071ac
Vlad Sirenko <sirenkovladd@gmail.com>
2024-11-04 13:31:34 -0800

    add zed to setup

--- setup/index.ts
+++ setup/index.ts
@@ -1,13 +1,13 @@
-import diff from 'fast-diff';
 import { lstat, readFile, readlink, rm, symlink } from 'node:fs/promises';
 import { resolve } from 'node:path';
+import diff from 'fast-diff';
 
 async function fileExist(path: string) {
   try {
-    return await lstat(path)
+    return await lstat(path);
   } catch (err) {
     if (err instanceof Error && 'code' in err && err.code === 'ENOENT') {
-      return false
+      return false;
     }
     throw err;
   }
@@ -15,10 +15,11 @@ async function fileExist(path: string) {
 
 function printDiff(patch: diff.Diff[]) {
   patch.forEach((part) => {
-    const color = part[0] === 1 ? '\x1b[32m' : part[0] === -1 ? '\x1b[31m' : '\x1b[0m'
-    process.stdout.write(color + part[1])
-  })
-  process.stdout.write('\x1b[0m')
+    const color =
+      part[0] === 1 ? '\x1b[32m' : part[0] === -1 ? '\x1b[31m' : '\x1b[0m';
+    process.stdout.write(color + part[1]);
+  });
+  process.stdout.write('\x1b[0m');
 }
 
 async function makeSoftLink(from: string, to: string) {
@@ -29,49 +30,55 @@ async function makeSoftLink(from: string, to: string) {
 
   console.log();
 
-  if (!await fileExist(from)) {
-    console.error(`File ${from} does not exist. Skipping...`)
-    return
+  if (!(await fileExist(from))) {
+    console.error(`File ${from} does not exist. Skipping...`);
+    return;
   }
 
-  console.log(`Creating soft link from ${from} to ${to}`)
+  console.log(`Creating soft link from ${from} to ${to}`);
 
-  const stat = await fileExist(to)
+  const stat = await fileExist(to);
   if (stat) {
-    const contentFrom = await readFile(from, 'utf-8')
-    const contentTo = await readFile(to, 'utf-8')
+    const contentFrom = await readFile(from, 'utf-8');
+    const contentTo = await readFile(to, 'utf-8');
 
-    const differences = diff(contentFrom, contentTo)
+    const differences = diff(contentFrom, contentTo);
     if (differences.length === 1) {
       if (stat.isSymbolicLink()) {
-        const link = await readlink(to)
+        const link = await readlink(to);
         if (link === from) {
-          console.log(`Soft link already exists.`)
+          console.log(`Soft link already exists.`);
         } else {
-          console.log(`Soft link already exists but points to a different file. ${link}`)
+          console.log(
+            `Soft link already exists but points to a different file. ${link}`,
+          );
         }
-        return
+        return;
       }
-      rm(to)
+      rm(to);
     } else {
-      console.log(`Files are different. Printing diff...`)
-      printDiff(differences)
+      console.log(`Files are different. Printing diff...`);
+      printDiff(differences);
 
-      console.log(`Do you want to overwrite ${to}?`)
+      console.log(`Do you want to overwrite ${to}?`);
 
-      return
+      return;
     }
   }
   // Create soft link
-  await symlink(from, to)
+  await symlink(from, to);
 
-  console.log(`Soft link created.`)
+  console.log(`Soft link created.`);
 }
 
 async function main() {
-  await makeSoftLink('./config/mise.toml', '~/.config/mise/config.toml')
-  await makeSoftLink('./.zshrc', '~/.zshrc')
-  await makeSoftLink('./config/lazygit.yml', '~/Library/Application\ Support/lazygit/config.yml')
+  await makeSoftLink('./config/mise.toml', '~/.config/mise/config.toml');
+  await makeSoftLink('./.zshrc', '~/.zshrc');
+  await makeSoftLink(
+    './config/lazygit.yml',
+    '~/Library/Application Support/lazygit/config.yml',
+  );
+  await makeSoftLink('./zed/settings.json', '~/.config/zed/settings.json');
 }
 
 main();
