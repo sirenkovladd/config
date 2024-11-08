530f3b8c714d2dbdea3b0097e571ef06e9778fc9
Vlad Sirenko <sirenkovladd@gmail.com>
2024-11-07 19:23:25 -0800

    optimization fish

--- fish/config.fish
+++ fish/config.fish
@@ -8,20 +8,3 @@ if status is-interactive
     mise activate fish | source
     starship init fish | source
 end
-
-alias ssh="kitten ssh"
-alias ls="eza --icons"
-alias ll="ls -lohUmZ --git --time-style=long-iso --no-permissions"
-alias du="ncdu"
-alias lg="lazygit"
-alias cat="bat -P"
-alias clear="printf '\033[2J\033[3J\033[1;1H'"
-alias kitty-upgrade="curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin"
-
-# K8S
-alias kx="kubectx"
-alias kn="kubens"
-
-alias m="mise"
-alias mr="m run"
-alias zedtmp="zed $(mktemp -d)"
--- fish/init.sh
+++ fish/init.sh
@@ -11,3 +11,20 @@ wget https://raw.githubusercontent.com/eza-community/eza/refs/heads/main/complet
 bun completions
 ln -s ~/.local/share/mise/installs/bat/latest/autocomplete/bat.fish .config/fish/completions/bat.fish
 ln -s ~/.local/share/mise/installs/ripgrep/latest/complete/rg.fish .config/fish/completions/rg.fish
+
+alias -s ssh="kitten ssh"
+alias -s ls="eza --icons"
+alias -s ll="ls -lohUmZ --git --time-style=long-iso --no-permissions"
+alias -s du="ncdu"
+alias -s lg="lazygit"
+alias -s cat="bat -P"
+alias -s clear="printf '\033[2J\033[3J\033[1;1H'"
+alias -s kitty-upgrade="curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin"
+
+# K8S
+alias -s kx="kubectx"
+alias -s kn="kubens"
+
+alias -s m="mise"
+alias -s mr="m run"
+alias -s zedtmp="zed $(mktemp -d)"
