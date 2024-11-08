8b62b0bd36470e1c2705ffeb2e26072e7a82aae9
Vlad Sirenko <sirenkovladd@gmail.com>
2024-11-07 19:44:12 -0800

    minor change

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,9 @@
 # export PATH="/opt/homebrew/opt/llvm@15/bin:$PATH"
 # export PATH="/opt/homebrew/opt/llvm@16/bin:$PATH"
 
-set -x GPG_TTY $(tty)
+# set -x GPG_TTY $(tty)
 
+mise activate fish | source
 if status is-interactive
-    set -U fish_greeting ""
-    mise activate fish | source
     starship init fish | source
 end
