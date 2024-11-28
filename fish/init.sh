2aadb9612f1189f71a9ec45bcac7a0b2876b3c2a
Vlad Sirenko <sirenkovladd@gmail.com>
2024-11-28 01:21:41 -0800

    add neovim to mise

--- fish/init.sh
+++ fish/init.sh
@@ -5,8 +5,8 @@ set -U GIT_EXTERNAL_DIFF difft
 ln -fs $(pwd)/fish/config.fish ~/.config/fish/config.fish
 
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-docker completion fish > .config/fish/completions/docker.fish
-mise completion fish > ~/.config/fish/completions/mise.fish
+docker completion fish >.config/fish/completions/docker.fish
+mise completion fish >~/.config/fish/completions/mise.fish
 wget https://raw.githubusercontent.com/eza-community/eza/refs/heads/main/completions/fish/eza.fish -O .config/fish/completions/eza.fish
 bun completions
 ln -s ~/.local/share/mise/installs/bat/latest/autocomplete/bat.fish .config/fish/completions/bat.fish
@@ -15,7 +15,7 @@ ln -s ~/.local/share/mise/installs/ripgrep/latest/complete/rg.fish .config/fish/
 alias -s ssh="kitten ssh"
 alias -s ls="eza --icons"
 alias -s ll="ls -lohUmZ --git --time-style=long-iso --no-permissions"
-alias -s du="ncdu"
+alias -s du="dua i"
 alias -s lg="lazygit"
 alias -s cat="bat -P"
 alias -s clear="printf '\033[2J\033[3J\033[1;1H'"
