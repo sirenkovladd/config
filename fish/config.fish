cffcbd2fb7d2e4923f1c2d389a39a72bc4f9fe2d
Vlad Sirenko <sirenkovladd@gmail.com>
2025-02-11 23:09:19 -0800

    add claude config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 # export PATH="/opt/homebrew/opt/llvm@15/bin:$PATH"
 # export PATH="/opt/homebrew/opt/llvm@16/bin:$PATH"
+# aes -in backup_documents_22_07_24.zip -out backup_documents_22_07_24.zip.aes
+# aes -d -in backup_documents_22_07_24.zip -out backup_documents_22_07_24.zip.aes
 
 # set -x GPG_TTY $(tty)
 
@@ -7,3 +9,6 @@ mise activate fish | source
 if status is-interactive
     starship init fish | source
 end
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/vlad/.lmstudio/bin
